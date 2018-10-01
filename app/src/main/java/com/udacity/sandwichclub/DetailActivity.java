19b0f131a4cc7d7a15a81626f6abd396dd04d6e0
Richard Lu <rlu@jimmyjohns.com>
2018-09-30 22:22:47 -0500

    [DetailActivityStuff] link the views to their layout references.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,30 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
+    private TextView akaTextView;
+    private TextView ingredientsTextView;
+    private TextView placeOfOriginTextView;
+    private TextView descriptionTextView;
+
+    private LinearLayout ingredientsLayout;
+    private LinearLayout akaLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +56,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +69,35 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        populateAliasTextView(sandwich);
+        populateIngredientsTextView(sandwich);
+        populatePlaceOfOriginTextView(sandwich);
+        populateDescriptionTextView(sandwich);
     }
+
+    private void populateAliasTextView(Sandwich sandwich) {
+        if (!sandwich.getAlsoKnownAs().isEmpty()) {
+            akaTextView = findViewById(R.id.also_known_tv);
+        } else {
+            akaLayout = findViewById(R.id.aka_container);
+        }
+    }
+
+    private void populateIngredientsTextView(Sandwich sandwich) {
+        if (!sandwich.getIngredients().isEmpty()) {
+            ingredientsTextView = findViewById(R.id.ingredients_tv);
+        } else {
+            ingredientsLayout = findViewById(R.id.ingredients_container);
+        }
+    }
+
+    private void populatePlaceOfOriginTextView(Sandwich sandwich) {
+        placeOfOriginTextView = findViewById(R.id.origin_tv);
+    }
+
+    private void populateDescriptionTextView(Sandwich sandwich) {
+        descriptionTextView = findViewById(R.id.description_tv);
+    }
+
 }
